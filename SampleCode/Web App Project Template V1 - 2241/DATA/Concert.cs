57d039bc39cc864215aba550b28948048be9ec1b
Ruyuan Sun <rsun35@myseneca.ca>
2024-06-11 16:34:38 -0400

    add unzipped template

---
+++ SampleCode/Web App Project Template V1 - 2241/DATA/Concert.cs
@@ -0,0 +1,52 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace $safeprojectname$.Data
+{
+    [Table("Concert")]
+    public class Concert
+    {
+
+        #region Columns
+
+        public int ConcertId { get; set; }
+
+        [Required]
+        [StringLength(128)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(80)]
+        public string Company { get; set; }
+
+        [StringLength(70)]
+        public string Address { get; set; }
+
+        [StringLength(40)]
+        public string City { get; set; }
+
+        [StringLength(40)]
+        public string State { get; set; }
+
+        [StringLength(40)]
+        public string Country { get; set; }
+
+        [StringLength(10)]
+        public string PostalCode { get; set; }
+
+        [StringLength(24)]
+        public string Phone { get; set; }
+
+        [StringLength(100)]
+        public string Email { get; set; }
+
+        [StringLength(100)]
+        public string Website { get; set; }
+
+        public DateTime ConcertDate { get; set; }
+
+        #endregion
+
+    }
+}
